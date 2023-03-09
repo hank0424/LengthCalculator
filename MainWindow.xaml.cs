1ae69c62b3300f40ee03c94f9494ce2fede500e7
hank0424 <125251101+hank0424@users.noreply.github.com>
2023-03-09 17:36:26 +0800

    轉換程式改良
    新增:
    清除按鍵功能
    改動:
    長度單位轉換的程式判別機制

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,16 +24,36 @@ namespace LengthCalculator
         {
             InitializeComponent();
         }
-
+        string strInput; 
+        double douOutput; 
         private void txtCM_KeyUp(object sender, KeyEventArgs e)
         {
-            double douCM; 
-            douCM = Convert.ToDouble(txtCM.Text);
-            txtM.Text = string.Format("{0:0.##########}", douCM / 100);
-            txtKM.Text = string.Format("{0:0.##########}", douCM / 100000);
-            txtIn.Text = string.Format("{0:0.##########}", douCM / 2.54);
-            txtFt.Text = string.Format("{0:0.##########}", douCM / 30.48);
-            txtYard.Text = string.Format("{0:0.##########}", douCM / 91.44);
+            strInput = txtCM.Text;
+            if (double.TryParse(strInput, out douOutput) == true)
+            {
+                txtM.Text = string.Format("{0:0.##########}", douOutput / 100);
+                txtKM.Text = string.Format("{0:0.##########}", douOutput / 100000);
+                txtIn.Text = string.Format("{0:0.##########}", douOutput / 2.54);
+                txtFt.Text = string.Format("{0:0.##########}", douOutput / 30.48);
+                txtYard.Text = string.Format("{0:0.##########}", douOutput / 91.44);
+            }
+            else
+            {
+
+                    txtInfo.Text = "請輸入數字";
+                    txtCM.Text = "";
+                
+            }
+        }
+
+        private void btnALLClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtCM.Text = "";
+            txtM.Text = "";
+            txtKM.Text = "";
+            txtIn.Text = "";
+            txtFt.Text = "";
+            txtYard.Text = "";
         }
     }
 }
